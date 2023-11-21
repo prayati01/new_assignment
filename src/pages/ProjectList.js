01a0dc2c416cf3bdf10f2c7c3647688068c000e0
Prayati Paliwal <88851694+prayati01@users.noreply.github.com>
2023-11-22 01:40:41 +0530

    Update ProjectList.js

--- src/pages/ProjectList.js
+++ src/pages/ProjectList.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { Link, useNavigate } from "react-router-dom"
 import Swal from 'sweetalert2'
 import Layout from "../components/Layout"
+import './Login.css'
 
 
 function ProjectList() {
